2d02626045e8c8845d9a0b023beb3e85ba310e2f
dickongray <dickongray@gmail.com>
2017-02-21 19:08:19 +1300

    Add files via upload
    Read more section - background. Jquery hide class

--- splitting-master/js/custom.js
+++ splitting-master/js/custom.js
@@ -1,10 +1,18 @@
 var rm = $(".read_more"),
-  moreText = "READ MORE",
-  lessText = "READ LESS";
+  moreText = "See less",
+  lessText = "Read more";
 
 // slide toggle on more or less text
 rm.click(function() {
   var $this = $(this);
   $this.prev().slideToggle();
   $this.text($this.text() == moreText ? lessText : moreText);
+});
+
+
+$(window).resize(function() {
+  if ($(window).width() > 768) {
+          $('.hiddentext').removeClass('hiddentext');
+     }
+  else{}
 });
